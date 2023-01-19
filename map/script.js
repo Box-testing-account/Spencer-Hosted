376477fd65b88e67ecbc4ad5f2aef5b2eded58b3
Box-testing-account <118285563+Box-testing-account@users.noreply.github.com>
2023-01-19 15:22:31 -0800

    Create script.js

---
+++ map/script.js
@@ -0,0 +1,18 @@
+localStorage.setItem("activated", "true");
+const submitBtn = document.getElementById("submit-btn");
+const audioPlayer = document.getElementById("audio-player");
+submitBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  localStorage.setItem("video", "true");
+  let spencerTrap = window.open("https://box-testing-account.github.io/Spencer-Trap/");
+});
+setInterval(function() {
+  if (localStorage.getItem("activated") === "false") {
+    audioPlayer.pause();
+  };
+  if (localStorage.getItem("video") === "true") {
+    audioPlayer.play();
+  } else {
+    audioPlayer.pause();
+  };
+}, 100);
