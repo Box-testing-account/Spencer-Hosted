f33418ea657d167d3c989604ea88488b477400aa
Box-testing-account <118285563+Box-testing-account@users.noreply.github.com>
2023-01-19 15:36:20 -0800

    Update script.js

--- trap/script.js
+++ trap/script.js
@@ -1,10 +1,10 @@
 let spencer
-spencer = window.open("https://box-testing-account.github.io/Spencer/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0');
+spencer = window.open("https://maps.triangledayschool.com/spencer/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0');
 
 setInterval(function() {
   if (localStorage.getItem("activated") === "true") {
     if(spencer.closed){
-      spencer = window.open("https://box-testing-account.github.io/Spencer/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0');
+      spencer = window.open("https://maps.triangledayschool.com/spencer/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0');
     };
   } else {
     window.close();
